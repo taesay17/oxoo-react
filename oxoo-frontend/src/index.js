83994410fa517daaa4ce312f8c6122c42f77fb9b
taesay17 <151002316+taesay17@users.noreply.github.com>
2025-12-18 23:44:36 +0600

    Fix CSS imports for GitHub Pages

--- oxoo-frontend/src/index.js
+++ oxoo-frontend/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/styles.css";
+import "./styles/home.css";
+import "./components/Navbar.css";
+import "./components/IslandsPreview.css";
+import "./components/VoiceAssistant.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
